eccb7a3ac9293b6a278f30b90758155d614b1005
JAntonio <judithantonio@email.arizona.edu>
2021-04-19 22:50:05 -0700

    fixing <a> href

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
     <div className="col-md-12 float-right">
       <br />
       <h2 id="date_time">Monday 3/29/2021, 5:00</h2>
-      <a id="git" href="#">
+      <a id="git" href="./">
         Checkout Git Repo
       </a>
     </div>
